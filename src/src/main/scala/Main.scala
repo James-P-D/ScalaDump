149ba1c1daed2a9930bb6a7b1ae3a276e9455757
James Dorrington <jdorrington@hotmail.com>
2020-08-04 19:59:53 +0100

    Checking in Main.scala

---
+++ src/src/main/scala/Main.scala
@@ -0,0 +1,11 @@
+import scala.io.StdIn.{readLine,readInt}
+import scala.math._
+import scala.collection.mutable.ArrayBuffer
+import java.io.PrintWriter
+import scala.io.Source
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+  }
+}
